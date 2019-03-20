e4e526b1fa5bc78cdec55210bbbf1578ba88f619
Nikola Tanković <nikola.tankovic@gmail.com>
2019-03-20 22:54:41 +0100

    Update Flask servisa

--- 01/server_dynamic_rest_flask.py
+++ 01/server_dynamic_rest_flask.py
@@ -2,29 +2,39 @@ from flask import Flask, request, Response
 from flask import jsonify
 
 
-app = Flask(__name__)
-studenti = [{"id": "0", "ime":"hrvoje", "prezime":"horvat"}]
+app = Flask(__name__)  # __name__ sadrži naziv trenutnog fajla
+studenti = [
+    {"id": 1, "ime": "Hrvoje", "prezime": "Horvat"},
+    {"id": 2, "ime": "Mirko", "prezime": "Mirkić"}
+]
+
+
+@app.route('/studenti')  # dekorator koji navodi Flasku koji URL obrađuje metoda
+def dohvati_studente():
+    return jsonify({"answer": studenti})
 
 
 @app.route('/studenti', methods=['POST'])
 def spremi_studenta():
     s = request.get_json()
+    # ovdje bi trebalo provjeriti jel sve OK
+    print("Dobio sam studenta: ", s)
     studenti.append(s)
-    return Response(status=201)
+    r = Response(status=201)
+    r.set_data("Created.")
+    return r
 
-@app.route('/studenti/<id>')
-def dohvati_studenta(id):
+
+@app.route('/studenti/<id_studenta>')
+def dohvati_studenta(id_studenta):
     answer = None
     for s in studenti:
-        if "id" in s and s["id"] == id:
+        # može li bolje?
+        if "id" in s and s["id"] == int(id_studenta):
             answer = s
             break
 
-    return jsonify({"answer": answer })
-
-@app.route('/studenti')
-def dohvati_studente():
-    return jsonify({"answer":studenti})
+    return jsonify({"answer": answer})
 
 
 # if __name__ == '__main__':
